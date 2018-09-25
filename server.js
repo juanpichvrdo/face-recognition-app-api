855035c7735755af7f314dde3f332e3d6eec790b
Juan Daniel Pichardo Peña <jpichardopena@gmail.com>
2018-09-24 23:22:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const db = knex({
   client: "pg",
   connection: {
     host: "127.0.0.1",
-    user: "postgres",
-    password: "1234",
+    user: "****",
+    password: "*********",
     database: "'smart-brain'"
   }
 });
